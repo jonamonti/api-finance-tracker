ca67764878e20e0199e473022644b49c6bde08be
jonamonti <jonathanmonti@hotmail.com>
2023-09-16 11:32:42 -0300

    feat: Transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { trxCategories, trxTypes } from "../utils/const.js";
+
+const TransactionSchema = new mongoose.Schema({
+  userID: mongoose.Schema.Types.ObjectId,
+  type: {
+    type: String,
+    enum: trxTypes,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true,
+    set: (v) => v.toFixed(2) // to store amount rounded to 2 decimals
+  },
+  description: {
+    type: String,
+    default: '',
+    max: 30
+  },
+  category: {
+    type: String,
+    enum: trxCategories,
+    required: true
+  }
+},
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model('Transaction', TransactionSchema);
+export default Transaction;
