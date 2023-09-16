3ca54cd1ca906536fe341602d850af0684419024
jonamonti <jonathanmonti@hotmail.com>
2023-09-16 14:02:20 -0300

    feat: post, put and delete trx endpoints

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import { verifyToken } from '../middleware/auth.js';
-import { getTrxs } from '../controllers/transaction.js';
+import { getTrxs, updateTrx, createTrx, deleteTrx } from '../controllers/transaction.js';
 
 const router = express.Router();
 
 // GET
 // get user transactions
 router.get('/:id', verifyToken, getTrxs); // Trxs as transactions
+
+// POST
+// add new transaction
+router.post('/', verifyToken, createTrx);
+
+// UPDATE
+// edit transaction info
+router.put('/', verifyToken, updateTrx);
+
+// DELETE
+router.delete('/', verifyToken, deleteTrx)
+
 
 export default router;
